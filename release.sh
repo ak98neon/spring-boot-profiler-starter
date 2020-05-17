f74be270fe18fb7ea7c0a5c9dd9da827575e800a
ak98neon <ak98nneon@gmail.com>
2020-05-17 19:08:31 +0300

    Upgrade version, create release script

---
+++ release.sh
@@ -0,0 +1,6 @@
+#bin/sh
+
+mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit
+mvn clean deploy -DperformRelease=true -P release:prepare
+git commit -m "Release new version"
+git push -u origin master
