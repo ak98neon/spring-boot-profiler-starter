712fa921a19c0fed033a7f8ae4765226a407e229
ak98neon <ak98nneon@gmail.com>
2020-05-17 19:17:23 +0300

    Release new version

--- release.sh
+++ release.sh
@@ -2,5 +2,6 @@
 
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit
 mvn clean deploy -DperformRelease=true -P release:prepare
+git add --all
 git commit -m "Release new version"
 git push -u origin master
